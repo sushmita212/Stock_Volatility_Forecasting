67bafdf744d6c902cb579b65870b454d7ae40cfd
sushmita212 <121579685+sushmita212@users.noreply.github.com>
2025-07-30 16:10:38 -0700

    HV v GARCH

--- hvPred.py
+++ hvPred.py
@@ -1,34 +1,37 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 class HVmodel:
     def __init__(self, stock_data, train_size=0.8):
         self.stock_data = stock_data
         self.train_size = train_size
 
-    def train_test_split(self):
-        cutoff = int(len(self.stock_data) * self.train_size)
-        y_test = self.stock_data["returns"].iloc[cutoff:]
-        return y_test
     
     def HVpred(self):
-        y_test = self.train_test_split()
+        cutoff = int(len(self.stock_data) * self.train_size)
+        self.y_test = self.stock_data["returns"].iloc[cutoff:]
+        
+        # Shift HV prediction by 1 because for step t+1 we can only use data up to t
         hv=self.stock_data['returns'].rolling(window=21).std().shift(1).dropna()
-        hv=hv.iloc[-len(y_test):]
-    
-        return hv
+        hv=hv.iloc[-len(self.y_test):]
+        self.hv_pred = hv
 
-    def plot_results(self,  hv):
-        y_test = self.train_test_split()
-        h_vol= pd.Series(hv, index=y_test.index, name='Predicted Volatility')
+    def plot_results(self):
+        if self.hv_pred is None or self.y_test is None:
+            raise ValueError("Must call HVpred() before plotting.")
+        
+        h_vol= pd.Series(self.hv_pred, index=self.y_test.index, name='Predicted Volatility')
+
         test_vol= (1/(4*np.log(2))*(((np.log(self.stock_data['high']/self.stock_data['low'])*100)**2)).rolling(window=1).mean())
-        test_vol=np.sqrt(test_vol).iloc[-len(y_test):]
+        test_vol=np.sqrt(test_vol).iloc[-len(self.y_test):]
+
+
         plt.figure(figsize=(10, 6))
-        plt.plot(y_test.index, y_test, color='lightgray', label='Log Returns')
-        plt.plot(y_test.index, h_vol, color='black', label='Predicted Volatility')
-        plt.plot(test_vol.index, test_vol, color='red', label='Actual Volatility')
+        plt.plot(self.y_test.index, self.y_test, color='lightgray', label='Log Returns')
+        plt.plot(self.y_test.index, h_vol, color='black', label='Predicted Volatility')
+        plt.plot(test_vol.index, test_vol, color='red', label='Realized Volatility')
         plt.legend()
         plt.title('GARCH Model Walk Forward Prediction')
         plt.xlabel('Date')
@@ -36,18 +39,20 @@ class HVmodel:
         plt.show()
 
 
-    def evaluate_model(self, hv, print_results=False    ):
-        y_test = self.train_test_split()
-        h_vol = pd.Series(hv, index=y_test.index, name='Predicted Volatility')
-        test_vol = (1/(4*np.log(2))*(((np.log(self.stock_data['high']/self.stock_data['low'])*100)**2)).rolling(window=1).mean())
-        test_vol = np.sqrt(test_vol).iloc[-len(y_test):]
+    def compute_error_metrics(self):
+        if self.hv_pred is None or self.y_test is None:
+            raise ValueError("Must call HVpred() before computing error metrics.")
         
+        h_vol = pd.Series(self.hv_pred, index=self.y_test.index, name='Predicted Volatility')
+       
+        test_vol = (1/(4*np.log(2))*(((np.log(self.stock_data['high']/self.stock_data['low'])*100)**2)).rolling(window=1).mean())
+        test_vol = np.sqrt(test_vol).iloc[-len(self.y_test):]
+
         mae = mean_absolute_error(test_vol, h_vol)
         mse = mean_squared_error(test_vol, h_vol)
         rmse = np.sqrt(mse)
         qlike = np.mean(np.log(h_vol**2)+(test_vol/h_vol)**2)
 
-        if print_results:
-            print(f"MAE: {mae:.4f}, MSE: {mse:.4f}, RMSE: {rmse:.4f}, QLIKE: {qlike:.4f}")
-        return mae, mse, rmse, qlike
+        self.error_metrics = {"MAE":round(mae,3), "MSE": round(mse,3), "RMSE": round(rmse,3), "QLIKE": round(qlike,3)}
+        return self.error_metrics
     
